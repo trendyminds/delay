57395d6ed1a4ee7e236de1c5818c5addd04dd60e
Aaron Bushnell <aaron.bushnell@gmail.com>
2020-04-24 13:17:53 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * A promise-based wrapper around `setTimeout()`. Allows you to delay by an arbitrary number of ms
+ * Ex: await delay(2000)
+ *
+ * @param {number} ms Number of milliseconds to wait until the promise is fulfilled
+ */
+function delay(ms) {
+  return new Promise(res => setTimeout(res, ms));
+}
+
+export default delay;
